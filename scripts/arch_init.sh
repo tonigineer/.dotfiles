4114006f190179033e13d145c97c822adf470fab
Z790E_WIN11 <toni@lubiniecki.de>
2024-05-08 21:06:37 +0200

    fixes

--- scripts/arch_init.sh
+++ scripts/arch_init.sh
@@ -6,24 +6,24 @@ echo ""
 
 echo -n ">> Enable NetworkManager "
 systemctl enable NetworkManager
-[[ $? -eq 0 ]] && "✓" || "✕" 
+[[ $? -eq 0 ]] && echo "✓" || echo "✕" 
 
 
 echo -n ">> Setting timezone "
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-[[ $? -eq 0 ]] && "✓" || "✕" 
+[[ $? -eq 0 ]] && echo "✓" || echo "✕" 
 
 
 echo -n ">> Setting to HW clock "
 hwclock --systohc
-[[ $? -eq 0 ]] && "✓" || "✕" 
+[[ $? -eq 0 ]] && echo "✓" || echo "✕" 
 
 
 echo -n ">> Setting Locale "
 sed -i 's/#en_US.UTF-8/en_US.UTF-8/g' /etc/locale.gen  # or use vim
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
-[[ $? -eq 0 ]] && "✓" || "✕" 
+[[ $? -eq 0 ]] && echo "✓" || echo "✕" 
 
 
 echo -n ">> Setting Hostname"
@@ -32,4 +32,4 @@ echo "
 127.0.0.1 localhost
 ::1 localhost
 127.0.0.1 Z790E.localadmin Z790E" >> /etc/hosts
-[[ $? -eq 0 ]] && "✓" || "✕" 
+[[ $? -eq 0 ]] && echo "✓" || echo "✕" 
