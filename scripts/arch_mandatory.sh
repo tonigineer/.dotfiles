dac87661c77b4a4d21103d085b94c17a602abe0e
Z790E_WIN11 <toni@lubiniecki.de>
2024-05-08 21:57:36 +0200

    further changes

--- scripts/arch_mandatory.sh
+++ scripts/arch_mandatory.sh
@@ -3,26 +3,21 @@
 echo "Initial settings for Arch Linux"
 
 
-echo -n ">> Install yay "
-sudo pacman -S base-devel git vim
-cd /opt
-sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R toni:wheel ./yay
-cd yay
-makepkg -si
-
-
-echo -n ">> Configure pacman"
+echo ">> Configure pacman"
 sudo sed -i 's\#Color\Color\g' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads = 5/ILoveCandy\nParallelDownloads = 10/g' /etc/pacman.conf
 
+sudo sed -i 's\#[multilib]\[multilib]\g' /etc/pacman.conf
+sudo sed -i 's\#Include = /etc/pacman.d/mirrorlist\Include = /etc/pacman.d/mirrorlist\g' /etc/pacman.conf
 
-echo -n ">> Configure VIM"
+
+echo ">> Configure VIM"
 echo "syntax on
 filetype on
 set noswapfile
 set cursorcolumn
-set number
+set relativenumber
+set rnu
 set expandtab
 set tabstop=4
 set softtabstop=4
@@ -32,3 +27,13 @@ set textwidth=80
 set nobackup
 set hlsearch
 set showmatch" > ~/.vimrc
+sudo ln -s ~/.vimrc /root/.vimrc
+
+
+echo -n ">> Install yay "
+sudo pacman -S base-devel git vim
+cd /opt
+sudo git clone https://aur.archlinux.org/yay.git
+sudo chown -R toni:wheel ./yay
+cd yay
+makepkg -si
