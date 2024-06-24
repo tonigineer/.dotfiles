f4ceb2dbd2be4448dd0190a873c44e2532931e85
Toni Lubiniecki <33184886+tonigineer@users.noreply.github.com>
2024-06-24 23:07:18 +0200

    skip yay if installed

--- scripts/arch_mandatory.sh
+++ scripts/arch_mandatory.sh
@@ -2,12 +2,11 @@
 
 echo "Initial settings for Arch Linux"
 
-
 echo ">> Configure pacman"
 sudo sed -i 's\#Color\Color\g' /etc/pacman.conf
 sudo sed -i 's/#ParallelDownloads = 5/ILoveCandy\nParallelDownloads = 10/g' /etc/pacman.conf
-
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+
 
 echo ">> Configure VIM"
 echo "syntax on
@@ -27,11 +26,14 @@ set hlsearch
 set showmatch" > ~/.vimrc
 sudo ln -s ~/.vimrc /root/.vimrc
 
+if [ ! -d "/opt/yay" ]; then
+  echo -n ">> Install yay "
+  sudo pacman -S base-devel git vim
+  cd /opt
+  sudo git clone https://aur.archlinux.org/yay.git
+  sudo chown -R toni:wheel ./yay
+  cd yay
+  makepkg -si
+fi
 
-echo -n ">> Install yay "
-sudo pacman -S base-devel git vim
-cd /opt
-sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R toni:wheel ./yay
-cd yay
-makepkg -si
+yay -Syyu
